f54973fa51ab4228f65fc26426378be4073f09b6
Sofia <sofia.demarco.en@gmail.com>
2026-05-22 18:52:28 -0700

    completed step 1

--- __tests__/lab7.test.js
+++ __tests__/lab7.test.js
@@ -21,7 +21,7 @@ describe('Basic user flow for Website', () => {
   // Check to make sure that all 20 <product-item> elements have data in them
   // We use .skip() here because this test has a TODO that has not been completed yet.
   // Make sure to remove the .skip after you finish the TODO. 
-  it.skip('Make sure <product-item> elements are populated', async () => {
+  it('Make sure <product-item> elements are populated', async () => {
     console.log('Checking to make sure <product-item> elements are populated...');
 
     // Start as true, if any don't have data, swap to false
@@ -37,22 +37,18 @@ describe('Basic user flow for Website', () => {
 
     console.log(`Checking product item 1/${prodItemsData.length}`);
 
-    // Make sure the title, price, and image are populated in the JSON
-    firstValue = prodItemsData[0];
-    if (firstValue.title.length == 0) { allArePopulated = false; }
-    if (firstValue.price.length == 0) { allArePopulated = false; }
-    if (firstValue.image.length == 0) { allArePopulated = false; }
+    for (let i = 0; i < prodItemsData.length; i++) {
+      const currentItem = prodItemsData[i];
+      
+      // Make sure the title, price, and image are populated in the JSON
+      if (currentItem.title.length == 0) { allArePopulated = false; }
+      if (currentItem.price.length == 0) { allArePopulated = false; }
+      if (currentItem.image.length == 0) { allArePopulated = false; }
+    }
 
     // Expect allArePopulated to still be true
     expect(allArePopulated).toBe(true);
-
-    /**
-    **** TODO - STEP 1 ****
-    * Right now this function is only checking the first <product-item> it found, make it so that
-      it checks every <product-item> it found
-    * Remove the .skip from this it once you are finished writing this test.
-    */
-
+    
   }, 10000);
 
   // Check to make sure that when you click "Add to Cart" on the first <product-item> that
